0c0cf09a1ca1995ec2e92a6071d05b9a87a3a72f
kosklain <kosklain@gmail.com>
2013-12-03 00:47:07 +0100

    Clean algorithm add

--- dummy.py
+++ dummy.py
@@ -34,30 +34,19 @@ def check_testing_repeated():
 
 def check_train_repeated():
     csv_reader_train = csv.reader(open("Train.csv"))
-    next(csv_reader_train)
-    train_titles = [line[1] for line in csv_reader_train]
-
+    csv_reader_train2 = csv.writer(open("Train_clean2.csv", "w"))
+    titles = []
+    repeated = 0
+    for i, line in enumerate(csv_reader_train):
+        if line[1] not in titles:
+            csv_reader_train2.writerow(line)
+            titles.append(line[1])
+        else:
+            repeated += 1
+            if (repeated % 1000) == 0:
+                print "Repeated %d in %d" % (repeated, i)
     print "Done with titles in training"
-    csv_reader_train2 = csv.reader(open("Train.csv"))
-    next(csv_reader_train2)
-    test_titles = [(i, line[1]) for i, line in enumerate(csv_reader_train2)]
-    print "Done with the titles in testing"
+    csv_reader_train2.close()
+    csv_reader_train.close()
 
-
-    def is_repeated(title):
-        if (title[0] % 10000) == 0:
-            print "Round %d" % title[0]
-        if train_titles.count(title[1]) > 1:
-            print "STOPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP!"
-            sys.exit(1)
-
-    pool = Pool(processes=4)
-    result = pool.map(is_repeated, test_titles)
-
-    final_index_array_test = np.array([element[0] for element in result if element != None])
-    final_index_array_train = np.array([element[1] for element in result if element != None])
-    print "Done, %d out of %d" % (len(final_index_array_test), len(result))
-    np.save("repeated_test", final_index_array_test)
-    np.save("repeated_train", final_index_array_train)
-
-check_testing_repeated()
+check_train_repeated()
